25da625acac9647d08c9ee475a083904c48a4df8
Samrat-ahammed <samratahammed29@gmail.com>
2024-08-27 12:14:08 +0600

    add card and fixed issue

--- src/app/(dashboard)/admin/page.js
+++ src/app/(dashboard)/admin/page.js
@@ -1,5 +1,23 @@
-import React from "react";
+import UserCard from "@/components/UserCard";
+import Image from "next/image";
 
-export default function AdminPage() {
-  return <div>Admin</div>;
-}
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT  */}
+      <div className="w-full lg:w-2/3">
+        {/* User Card  */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard name={"teacher"} />
+          <UserCard name={"Student"} />
+          <UserCard name={"parent"} />
+          <UserCard name={"staff"} />
+        </div>
+      </div>
+      {/* RIGHT  */}
+      <div className="w-full lg:w-1/3">ok</div>
+    </div>
+  );
+};
+
+export default AdminPage;
--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,4 +1,8 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +15,21 @@ export default function DashBoardLayout({ children }) {
   return (
     <div className="h-screen flex flex-row">
       {/* left */}
-      <div className="w-1/6">l</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+        <Menu />
+      </div>
       {/* right */}
-      <div className="w-5/6">r</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
